4195f5be749e769b6ab9029cc3fac2f5576c2971
Umar Farooque <umarf2212@gmail.com>
2018-10-20 15:07:28 +0530

    New commit2

--- envato-tracker/settings.py
+++ envato-tracker/settings.py
@@ -110,6 +110,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Deployment checklist
 """
